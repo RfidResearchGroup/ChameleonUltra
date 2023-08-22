961b8a58f35d925dae26bbad4c262146f4710ebf
mkozlowski <10508687+m-kozlowski@users.noreply.github.com>
2023-08-22 11:37:12 +0200

    Faster COM port detection

--- software/script/chameleon_cli_unit.py
+++ software/script/chameleon_cli_unit.py
@@ -198,7 +198,8 @@ class HWConnect(BaseCLIUnit):
                             PSHEXE=fn
                             break
                     if PSHEXE:
-                        process = subprocess.Popen([PSHEXE,"Get-CimInstance -ClassName Win32_serialport | Where-Object {$_.PNPDeviceID -like '*VID_6868&PID_8686*'} | Select -expandproperty DeviceID"],stdout=subprocess.PIPE);
+                        #process = subprocess.Popen([PSHEXE,"Get-CimInstance -ClassName Win32_serialport | Where-Object {$_.PNPDeviceID -like '*VID_6868&PID_8686*'} | Select -expandproperty DeviceID"],stdout=subprocess.PIPE);
+                        process = subprocess.Popen([PSHEXE,"Get-PnPDevice -Class Ports -PresentOnly | where {$_.DeviceID -like '*VID_6868&PID_8686*'} | Select-Object FriendlyName | % FriendlyName | select-string COM\d+ |% { $_.matches.value }"],stdout=subprocess.PIPE);
                         res = process.communicate()[0]
                         _comport = res.decode('utf-8').strip()
                         if _comport:
