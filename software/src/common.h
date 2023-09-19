624e96aee94a31bcf21d041da3def68259740286
Philippe Teuwen <phil@teuwen.org>
2023-09-19 13:18:02 +0200

    fix compilation

--- software/src/common.h
+++ software/src/common.h
@@ -1,5 +1,5 @@
-#ifndef NESTED_H__
-#define NESTED_H__
+#ifndef COMMON_H__
+#define COMMON_H__
 
 uint64_t atoui(const char* str);
 void num_to_bytes(uint64_t n, uint32_t len, uint8_t* dest);
--- software/src/staticnested.c
+++ software/src/staticnested.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
-
+#include "common.h"
 #include "nested_util.h"
 
 int main(int argc, char *const argv[]) {
