963d4d62f1bc6da5a079ab7c67317c2530d2af95
Philippe Teuwen <phil@teuwen.org>
2023-09-25 23:32:01 +0200

    Deduce APP_FW_VER from git tag

--- firmware/application/src/app_config.h
+++ firmware/application/src/app_config.h
@@ -2,8 +2,11 @@
 
 
 // version code, 1byte, max = 0 -> 255
-#define APP_FW_VER_MAJOR   2
-#define APP_FW_VER_MINOR   0
+//#define APP_FW_VER_MAJOR   2
+//#define APP_FW_VER_MINOR   0
+#if !(defined APP_FW_VER_MAJOR && defined APP_FW_VER_MINOR)
+  #error You need to define APP_FW_VER_MAJOR and APP_FW_VER_MINOR
+#endif
 
 // Merge major and minor version code to U16 value.
 #define FW_VER_NUM VER_CODE_TO_NUM(APP_FW_VER_MAJOR, APP_FW_VER_MINOR)
