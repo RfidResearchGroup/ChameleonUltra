826776231711395a86b8f401f2cb22220ae194c4
merlokk <807634+merlokk@users.noreply.github.com>
2024-05-12 18:38:56 +0300

    em4100 import from flipper

---
+++ firmware/application/src/rfid/reader/lf/protocols/lfrfid_protocols.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <utils/protocol.h>
+#include "utils/t5577.h"
+
+typedef enum {
+    LFRFIDFeatureASK = 1 << 0, /** ASK Demodulation */
+    LFRFIDFeaturePSK = 1 << 1, /** PSK Demodulation */
+} LFRFIDFeature;
+
+typedef enum {
+    LFRFIDProtocolEM4100,
+    LFRFIDProtocolEM410032,
+    LFRFIDProtocolEM410016,
+    LFRFIDProtocolH10301,
+    LFRFIDProtocolIdteck,
+    LFRFIDProtocolIndala26,
+    LFRFIDProtocolIOProxXSF,
+    LFRFIDProtocolAwid,
+    LFRFIDProtocolFDXA,
+    LFRFIDProtocolFDXB,
+    LFRFIDProtocolHidGeneric,
+    LFRFIDProtocolHidExGeneric,
+    LFRFIDProtocolPyramid,
+    LFRFIDProtocolViking,
+    LFRFIDProtocolJablotron,
+    LFRFIDProtocolParadox,
+    LFRFIDProtocolPACStanley,
+    LFRFIDProtocolKeri,
+    LFRFIDProtocolGallagher,
+    LFRFIDProtocolNexwatch,
+    LFRFIDProtocolMax,
+} LFRFIDProtocol;
+
+extern const ProtocolBase* lfrfid_protocols[];
+
+typedef enum {
+    LFRFIDWriteTypeT5577,
+} LFRFIDWriteType;
+
+typedef struct {
+    LFRFIDWriteType write_type;
+    union {
+        LFRFIDT5577 t5577;
+    };
+} LFRFIDWriteRequest;
---
+++ firmware/application/src/rfid/reader/lf/protocols/protocol_em4100.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <utils/protocol.h>
+
+extern const ProtocolBase protocol_em4100;
+
+extern const ProtocolBase protocol_em4100_32;
+
+extern const ProtocolBase protocol_em4100_16;
