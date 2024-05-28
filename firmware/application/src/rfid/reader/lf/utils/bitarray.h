7e1842ae4de16bcab644f057e697c90301f7f79b
merlokk <807634+merlokk@users.noreply.github.com>
2024-05-28 11:01:43 +0300

    add bitarray

---
+++ firmware/application/src/rfid/reader/lf/utils/bitarray.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint8_t count : 2;
+    uint32_t value : 6;
+} BitArray;
+
+bool bitarray_get_bit(BitArray b, uint8_t bit_num, bool* bit);
+bool bitarray_set_bit(BitArray* b, uint8_t bit_num, bool bit);
+bool bitarray_add_bit(BitArray* b, bool bit);
+uint8_t bitarray_count(BitArray b);
+
+#ifdef __cplusplus
+}
+#endif
