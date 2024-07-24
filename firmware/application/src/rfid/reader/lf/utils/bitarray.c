93bdccf7118de11c8b706cea177bfe09db980f1b
merlokk <807634+merlokk@users.noreply.github.com>
2024-07-24 13:16:44 +0300

    fix bit_array

--- firmware/application/src/rfid/reader/lf/utils/bitarray.c
+++ firmware/application/src/rfid/reader/lf/utils/bitarray.c
@@ -17,7 +17,7 @@ bool bitarray_set_bit(BitArray* b, uint8_t bit_num, bool bit) {
     if (b == NULL || b->count < bit_num + 1)
         return false;
 
-    uint8_t t = (b->value | (1 << bit_num))
+    uint8_t t = (b->value | (1 << bit_num));
     if (!bit)
         t = t ^ (1 << bit_num);
 
@@ -28,12 +28,12 @@ bool bitarray_add_bit(BitArray* b, bool bit) {
     if (b == NULL || b->count == 4)
         return false;
 
-    uint8_t t = b->value << 1 | bit
+    uint8_t t = b->value << 1 | bit;
     b->value = t;
-    b->count++
+    b->count++;
     return true;
 }
 
 uint8_t bitarray_count(BitArray b) {
-    return b->count;
+    return b.count;
 }
