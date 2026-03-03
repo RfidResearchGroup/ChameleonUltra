202c6a677e8aefdd6c6115eb89fb41c628fc78d7
Jozef Bernadic <bernadic@gmail.com>
2026-03-03 16:12:40 +0100

    feat(lf): integrate ioProx into LF reader, emulation and T55xx writer pipeline

--- firmware/application/src/rfid/reader/lf/lf_reader_data.h
+++ firmware/application/src/rfid/reader/lf/lf_reader_data.h
@@ -19,6 +19,7 @@ uint32_t get_lf_counter_value(void);
 void clear_lf_counter_value(void);
 
 bool em410x_read(uint8_t *data, uint32_t timeout_ms);
+bool ioprox_read(uint8_t *data, uint8_t format_hint, uint32_t timeout_ms);
 bool hidprox_read(uint8_t *data, uint8_t format_hint, uint32_t timeout_ms);
 bool viking_read(uint8_t *data, uint32_t timeout_ms);
 
--- firmware/application/src/rfid/reader/lf/lf_reader_main.c
+++ firmware/application/src/rfid/reader/lf/lf_reader_main.c
@@ -6,6 +6,7 @@
 #include "lf_125khz_radio.h"
 #include "lf_reader_data.h"
 #include "protocols/em410x.h"
+#include "protocols/ioprox.h"
 #include "protocols/hidprox.h"
 #include "protocols/t55xx.h"
 #include "protocols/viking.h"
@@ -37,6 +38,46 @@ uint8_t scan_hidprox(uint8_t *data, uint8_t format_hint) {
         return STATUS_LF_TAG_OK;
     }
     return STATUS_LF_TAG_NO_FOUND;
+}
+
+/**
+ * @brief Search ioProx tag
+ * @param output 16 bytes ioprox_codec_t->data layout: version, facility code, card number, raw8
+ * @return STATUS_LF_TAG_OK on success
+ */
+uint8_t scan_ioprox(uint8_t *data, uint8_t format_hint) {
+    if (ioprox_read(data, format_hint, g_timeout_readem_ms)) {
+        return STATUS_LF_TAG_OK;
+    }
+    return STATUS_LF_TAG_NO_FOUND;
+}
+
+/**
+ * @brief Decode raw8 data to structured ioProx format
+ * @param raw8 Input 8 bytes
+ * @param output 16 bytes ioprox_codec_t->data layout: version, facility code, card number, raw8
+ * @return STATUS_SUCCESS on success
+ */
+uint8_t decode_ioprox_raw(uint8_t *raw8, uint8_t *output) {
+    if (ioprox_decode_raw_to_data(raw8, output)) {
+        return STATUS_SUCCESS;
+    }
+    return STATUS_CMD_ERR;
+}
+
+/**
+ * @brief Encode ioProx parameters to structured ioProx format
+ * @param ver Version byte
+ * @param fc Facility code byte
+ * @param cn Card number (16-bit)
+ * @param out 16 bytes ioprox_codec_t->data layout: version, facility code, card number, raw8
+ * @return STATUS_SUCCESS on success
+ */
+uint8_t encode_ioprox_params(uint8_t ver, uint8_t fc, uint16_t cn, uint8_t *out) {
+    if (ioprox_encode_params_to_data(ver, fc, cn, out)) {
+        return STATUS_SUCCESS;
+    }
+    return STATUS_CMD_ERR;
 }
 
 /**
@@ -115,6 +156,22 @@ uint8_t write_hidprox_to_t55xx(uint8_t format, uint32_t fc, uint64_t cn, uint32_
     if (blk_count == 0) {
         return STATUS_PAR_ERR;
     }
+    return write_t55xx(blks, blk_count, new_passwd, old_passwds, old_passwd_count);
+}
+
+/**
+ * Write ioprox card data to t55xx
+ */
+uint8_t write_ioprox_to_t55xx(uint8_t *card_data, uint8_t *new_passwd, uint8_t *old_passwds, uint8_t old_passwd_count) {
+    // Prepare T5577 block array: index 0 = config word, 1-2 = data blocks
+    uint32_t blks[3] = {0x00};
+
+    uint8_t blk_count = ioprox_t55xx_writer(card_data, blks);
+
+    if (blk_count == 0) {
+        return STATUS_PAR_ERR;
+    }
+
     return write_t55xx(blks, blk_count, new_passwd, old_passwds, old_passwd_count);
 }
 
