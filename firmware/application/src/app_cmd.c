ced3d0d4da76abcac2b300c6a6d25832fd1f394c
Niel Nielsen <nieldk@gmail.com>
2026-08-07 08:22:28 +0200

    Remove FDXB commands for PROJECT_CHAMELEON_ULTRA

--- firmware/application/src/app_cmd.c
+++ firmware/application/src/app_cmd.c
@@ -3053,7 +3053,7 @@ static data_frame_tx_t *cmd_processor_hf14a_4_debug_counters(uint16_t cmd, uint1
     return data_frame_make(cmd, STATUS_SUCCESS, 4, buf);
 }
 #endif
-#if defined(PROJECT_CHAMELEON_ULTRA)
+
 static data_frame_tx_t *cmd_processor_fdxb_scan(uint16_t cmd, uint16_t status, uint16_t length, uint8_t *data) {
     uint8_t card_buffer[2 + FDXB_DATA_SIZE] = {0x00};
     status = scan_fdxb(card_buffer);
@@ -3070,7 +3070,6 @@ static data_frame_tx_t *cmd_processor_fdxb_write_to_t55xx(uint16_t cmd, uint16_t
     status = write_fdxb_to_t55xx(data);
     return data_frame_make(cmd, status, 0, NULL);
 }
-#endif
 
 static cmd_data_map_t m_data_cmd_map[] = {
     {    DATA_CMD_GET_APP_VERSION,              NULL,                        cmd_processor_get_app_version,               NULL                   },
@@ -3147,10 +3146,8 @@ static cmd_data_map_t m_data_cmd_map[] = {
     {    DATA_CMD_PAC_WRITE_TO_T55XX,           before_reader_run,           cmd_processor_pac_write_to_t55xx,            NULL                   },
     {    DATA_CMD_JABLOTRON_SCAN,               before_reader_run,           cmd_processor_jablotron_scan,                NULL                   },
     {    DATA_CMD_JABLOTRON_WRITE_TO_T55XX,     before_reader_run,           cmd_processor_jablotron_write_to_t55xx,      NULL                   },
-#if defined(PROJECT_CHAMELEON_ULTRA)
     {    DATA_CMD_FDXB_SCAN,                    before_reader_run,           cmd_processor_fdxb_scan,                     NULL                   },
     {    DATA_CMD_FDXB_WRITE_TO_T55XX,          before_reader_run,           cmd_processor_fdxb_write_to_t55xx,           NULL                   },
-#endif
     {    DATA_CMD_IDTECK_WRITE_TO_T55XX,        before_reader_run,           cmd_processor_idteck_write_to_t55xx,         NULL                   },
     {    DATA_CMD_LF_T55XX_WRITE,               before_reader_run,           cmd_processor_lf_t55xx_write,                NULL                   },
     {    DATA_CMD_ADC_GENERIC_READ,             before_reader_run,           cmd_processor_generic_read,                  NULL                   },
